78b5c0b0f9ef29dbb4a975df229330f23d564421
rafaelmontgomery <rafael.montgomery@codengage.com>
2025-02-06 16:29:29 -0300

    (feat): modeling entities

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -0,0 +1,59 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+/// <summary>
+/// Represents the sale with store items
+/// This entity follows domain-driven design principles and includes business rules validation.
+/// </summary>
+public class Sale : BaseEntity
+{
+    /// <summary>
+    /// Gets the number of the sale.
+    /// Must not be null and will be generated automatically
+    /// </summary>
+    public long Number { get; set; }
+
+    /// <summary>
+    /// Gets the date and time when the sale was made.  
+    /// </summary>
+    public DateTime PurchasedAt { get; set; }
+
+    /// <summary>
+    /// Gets the customer who made a purchase.
+    /// </summary>
+    public User Customer { get; set; } = null!;
+
+    /// <summary>
+    /// Gets the customer id who made a purchase.
+    /// </summary>
+    public Guid CustomerId { get; set; }
+
+    /// <summary>
+    /// Gets the total sale amount
+    /// </summary>
+    public decimal TotalAmount { get; set; }
+
+    /// <summary>
+    /// Gets the branch where the sale was made
+    /// </summary>
+    public string Branch { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets the sale's items and products
+    /// </summary>
+    public IEnumerable<SaleItem> SaleItems { get; set; } = [];
+
+    /// <summary>
+    /// Gets the sale's total discount
+    /// </summary>
+    public decimal TotalDiscount => SaleItems.Sum(item => item.Discount);
+
+    /// <summary>
+    /// Initializes a new instance of the Sale class.
+    /// </summary>
+    public Sale()
+    {
+        PurchasedAt = DateTime.UtcNow;
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/SaleItem.cs
@@ -0,0 +1,25 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+public class SaleItem : BaseEntity
+{
+    public Sale Sale { get; set; } = null!;
+    public Guid SaleId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public Product Product { get; set; } = null!;
+    public Guid ProductId { get; set; }
+    public decimal TotalAmount => UnitPrice * Quantity;
+    public decimal Discount => GetDiscount();
+    public bool IsCancelled { get; set; }
+
+    private decimal GetDiscount()
+    {
+        return Quantity switch
+        {
+            >= 4 and < 10 => TotalAmount * 0.1m,
+            >= 10 and < 20 => TotalAmount * 0.2m,
+            _ => TotalAmount,
+        };
+    }
+}
